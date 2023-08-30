9f6835d8f47d094eed61202237c88208e406b10b
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-08-30 09:14:09 +0545

    able to jump

---
+++ lib/bird.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyBird extends StatelessWidget {
+  const MyBird({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      width: 60,
+      child: Image.asset("images/bird.png"),
+    );
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flappy_bird/bird.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,16 +11,116 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static double birdYAxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = birdYAxis;
+  bool gameHasStarted = false;
+
+  void jump() {
+    setState(() {
+      time = 0;
+      initialHeight = birdYAxis;
+    });
+  }
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(const Duration(milliseconds: 60), (timer) {
+      //this is a displacement formula where y=height and t= time y= -1/2gt^2 +vt
+      //at a certain time it goes to maximum height and comes down due to gravity
+
+      time += 0.04;
+      height = -4.9 * time * time + 2.8 * time; //v=2.8
+      setState(() {
+        birdYAxis = initialHeight - height;
+      });
+      //landing bird on ground
+      if (birdYAxis > 1) {
+        timer.cancel();
+        gameHasStarted = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Expanded(child: Container(color: Colors.blue,),),
-          Expanded(child: Container(color: Colors.green,),),
+          Expanded(
+            flex: 2,
+            child: GestureDetector(
+              //ontap we are giving this condition because we want to start timer only once instead of having
+              //timer started on every jump
+              onTap: () {
+                if (gameHasStarted == true) {
+                  jump();
+                } else {
+                  startGame();
+                }
+              },
+              child: AnimatedContainer(
+                alignment: Alignment(0, birdYAxis),
+                duration: const Duration(milliseconds: 0),
+                color: Colors.blue,
+                child: const MyBird(),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.brown,
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "SCORE",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("0",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35),),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "BEST",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("10",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35),),
+                    ],
+                  ),
+                  
+                ],
+              ),
+            ),
+          ),
         ],
       ),
-      
     );
   }
 }
